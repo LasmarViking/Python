e94f30a814700ecbf193fe302b76012199286efe
Daniel Lasmar <lasmar_daniel@hotmail.com>
2023-01-30 15:46:16 -0300

    Atualização no código

--- adivinhacao.py
+++ adivinhacao.py
@@ -13,5 +13,8 @@ chute = int(chute_str)
 if(numero_secreto == chute):
     print("Você acertou!")
 else:
-    print("Você errou!")
+    if(chute > numero_secreto):
+        print("Você errou! O seu chute foi maior do que o número secreto.")
+    if(chute < numero_secreto):
+        print("Você errou! O seu chute foi menor do que o número secreto.")
 print("Fim do jogo.")
