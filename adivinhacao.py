754e1f971cdb124a0a6500cdd9c2c21e53666cdf
Daniel Lasmar <lasmar_daniel@hotmail.com>
2023-02-01 12:27:03 -0300

    utilizando módulo random.randrange

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,7 +4,7 @@ print("********************************")
 print("Bem vindo ao jogo de adivinhação")
 print("********************************")
 
-numero_secreto = round(random.random() * 100)
+numero_secreto = random.randrange(1,101)
 total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
